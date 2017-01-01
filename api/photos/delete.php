95eaf90316e874a47a4cb85305840630b40ddcb8
Joe Jack <thimis88@gmail.com>
2017-01-01 04:36:06 +0000

    Fixed Headed to not display login when already logged in removed localhost ref from photo delete

--- api/photos/delete.php
+++ api/photos/delete.php
@@ -9,9 +9,9 @@ $PhotoClass = new Photo();
 $photo = $PhotoClass->singlePhoto($id);
 
 if (!($session->get('user.id') == $photo['user_id'])) {
-  header("Location: http://localhost/photos?message=notAllowed", true, 302);
+  header("Location: /photos?message=notAllowed", true, 302);
 
 } else {
   $deleteMessage = $PhotoClass->deletePhoto($id);
-  header("Location: http://localhost/photos?message=" . $deleteMessage, true, 302);
+  header("Location: /photos?message=" . $deleteMessage, true, 302);
 }
